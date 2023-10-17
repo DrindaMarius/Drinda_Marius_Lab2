f31fe7273a4fcee2ae468af532f21a8688bf728b
DrindaMarius <110119847+DrindaMarius@users.noreply.github.com>
2023-10-17 18:51:26 +0200

    Add project files.

---
+++ Drinda_Marius_Lab2/Data/Drinda_Marius_Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Drinda_Marius_Lab2.Models;
+
+namespace Drinda_Marius_Lab2.Data
+{
+    public class Drinda_Marius_Lab2Context : DbContext
+    {
+        public Drinda_Marius_Lab2Context (DbContextOptions<Drinda_Marius_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Drinda_Marius_Lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
